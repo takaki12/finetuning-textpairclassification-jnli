29fe331ecc7fb0422cc75fe62c221294390b1d82
Onaga Takaaki <tonaga@kanolab.net>
2023-04-16 05:56:52 +0900

    change key of dict

--- data_loader.py
+++ data_loader.py
@@ -2,6 +2,8 @@
 import torch
 import pytorch_lightning as pl
 from torch.utils.data import Dataset, DataLoader
+from transformers import BertJapaneseTokenizer
+import pandas as pd
 
 # データセットの作成
 class DatasetGenerator(Dataset):
@@ -20,23 +22,30 @@ class DatasetGenerator(Dataset):
             index (list): ほしいデータを指定 [a行:b行, c列:d列]
         """
         data_row = self.data.iloc[index]
-        text = data_row['text']
-        labels = data_row['label']
+        sentence_1 = data_row['sentence1']
+        sentence_2 = data_row['sentence2']
+
+        if data_row['label'] == 'entailment':
+            label = 2
+        elif data_row['label'] == 'neutral':
+            label = 1
+        elif data_row['label'] == 'contradiction':
+            label = 0
 
         encoding = self.tokenizer.encode_plus(
-            text=text,
-            add_special_tokens=True,
-            max_length=self.max_length,
-            padding="max_length",
-            truncation=True,
-            return_attention_mask=True,
-            return_tensors='pt',
+            text = sentence_1 + self.tokenizer.sep_token + sentence_2,# sepトークンで区切って文を結合
+            add_special_tokens = True,
+            max_length = self.max_length,
+            padding = "max_length",
+            truncation = True,
+            return_attention_mask = True,
+            return_tensors = 'pt',
         )
 
         return dict(
-            input_ids=encoding["input_ids"].flatten(),
-            attention_mask=encoding["attention_mask"].flatten(),
-            labels=torch.tensor(labels)
+            input_ids = encoding["input_ids"].flatten(),
+            attention_mask = encoding["attention_mask"].flatten(),
+            labels = torch.tensor(label)
         )
 
 # データローダの作成
@@ -68,3 +77,18 @@ class DataModuleGenerator(pl.LightningDataModule):
 
     def test_dataloader(self):
         return DataLoader(self.test_dataset, batch_size=self.test_batch_size)
+
+if __name__=='__main__':
+    # トークナイザの設定
+    tokenizer = BertJapaneseTokenizer.from_pretrained('cl-tohoku/bert-base-japanese-whole-word-masking')
+    # データを作る
+    data_dict = {'sentence1':['ドラえもんはどら焼きを食べている。'],'sentence2':['ネコ型ロボットは和菓子を食べられる。'], 'label':[1]}
+    # dataframe化
+    df = pd.DataFrame(data_dict)
+    # データセットの作成とセットアップ (本来はdfは訓練用と検証用とテスト用に分ける)
+    data_module = DataModuleGenerator(df, df, df, tokenizer, 32)
+    data_module.setup()
+
+    data = data_module.train_dataset[0]
+    # tensor化されたデータを取得できる。
+    print(data)
